55a38177b571c0de39228e58955c66cf9c017123
David Hansen <david@davidhansen.de>
2026-09-14 16:49:24 +0200

    layout and content update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,34 +1,9 @@
 import { defineConfig, fontProviders } from "astro/config";
 
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
-  fonts: [
-    {
-      provider: fontProviders.local(),
-      name: "NeueMontrealMedium",
-      cssVariable: "--font-neue-montreal-medium",
-      options: {
-        variants: [
-          {
-            src: ["./src/assets/fonts/PPNeueMontreal-Medium.woff2"],
-            weight: "medium",
-            style: "medium",
-          },
-        ],
-      },
-    },
-    {
-      provider: fontProviders.local(),
-      name: "NeueMontrealRegular",
-      cssVariable: "--font-neue-montreal-regular",
-      options: {
-        variants: [
-          {
-            src: ["./src/assets/fonts/PPNeueMontreal-Regular.woff2"],
-            weight: "normal",
-            style: "normal",
-          },
-        ],
-      },
-    },
-  ],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
